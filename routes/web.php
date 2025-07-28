735143ac5725376202e7cd0a0f1ca3398ccc47b5
Andndre <agungandre687@gmail.com>
2025-07-28 23:10:12 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use Illuminate\Support\Facades\Auth;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Auth::routes(['register' => false]);
